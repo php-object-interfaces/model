c02a2fb397d4888adfeb047a7d51e1f8827def9c
Nardberjean <Nardberjean@users.noreply.github.com>
2017-08-25 19:22:06 -0400

    Create TraversableListInterface.php

---
+++ TraversableListInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the PHP Object Interfaces package.
+ *
+ * (c) Jean-Bernard Addor
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+ 
+ namespace Psr\Container;
+ 
+interface TraversableListInterface
+{
+    public function traversableList(array $parameters);
+}
+
+// See https://github.com/php-fig for inspiration
